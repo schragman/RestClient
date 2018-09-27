fc66798549d7546899f70f2ad9a5d1277eebc72a
schragman <schragman@arcor.de>
2018-09-27 21:40:32 +0200

    [DEV]: First steps

---
+++ src/main/java/restClient/Book.java
@@ -0,0 +1,8 @@
+package restClient;
+
+
+public interface Book {
+
+	public String getTitle();
+
+}
--- src/main/java/restClient/FormHome.java
+++ src/main/java/restClient/FormHome.java
@@ -1,12 +1,44 @@
 package restClient;
 
+import java.io.Serializable;
+
 import javax.enterprise.context.SessionScoped;
+import javax.faces.event.ActionEvent;
 import javax.inject.Named;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 
 @SessionScoped
 @Named
-public class FormHome {
-	Client client = ClientBuilder.newClient();
+public class FormHome implements Serializable {
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+	private Book book;
+	private String bookTitle;
+	private long bookId;
+
+	public long getBookId() {
+		return bookId;
+	}
+
+
+	public void setBookId(long bookId) {
+		this.bookId = bookId;
+	}
+
+
+	public void doCallBook(ActionEvent pvEvent) {
+		Client client = ClientBuilder.newClient();
+		book = client.target("http://localhost:8080/ReferenceII/rest/books/2").request().get(Book.class);
+		bookTitle = book.getTitle();
+	}
+
+
+	public String getBookTitle() {
+
+		return (book == null) ? "Bitte gültige Id angeben" : bookTitle;
+
+	}
 }
